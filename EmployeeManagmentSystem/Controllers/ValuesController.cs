83246779fdd7d5603f7a5026c012c312084d098c
Awais Aslam <dbit.awaisaslam@gmail.com>
2025-07-15 10:28:07 +0500

    chnages

---
+++ EmployeeManagmentSystem/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagmentSystem.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public Task<IActionResult> Result()
+        {
+            var result = "Hello World";
+            return Task.FromResult<IActionResult>(Ok(result));
+        }
+
+
+    }
+}
